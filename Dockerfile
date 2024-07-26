b7a1f4f4e3de32c403e15e30107dc0d000f1a616
ST <suwara.paintt@gmail.com>
2024-07-26 23:46:55 +0700

    Add Unit Test and Integration Test

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9.7-slim
+
+RUN pip install -U pip
+RUN pip install pipenv
+
+WORKDIR /app
+RUN mkdir model_element
+
+COPY ["/web_service/Pipfile", "/web_service/Pipfile.lock", "./"]
+RUN pipenv install --system --deploy
+
+COPY ["/web_service/get_action.py", "./"]
+
+COPY ["/web_service/model_element/model.py", "/web_service/model_element/preprocess.py", "/web_service/model_element/qtable.bin", "./model_element/"]
+
+EXPOSE 9696
+
+ENTRYPOINT [ "gunicorn", "--bind=0.0.0.0:9696", "get_action:app" ]
---
+++ integration_test/test_docker.py
@@ -0,0 +1,29 @@
+import json
+import requests
+import sys
+sys.path.append("./web_service")
+from model_element.model import parameters
+
+input_json = ticker_info = {
+  'ticker': 'AAPL',
+  'target_date': '2024-01-05'
+}
+
+ticker_info = {
+  'ticker': 'AAPL',
+  'target_date': '2024-01-05'
+}
+
+url = 'http://localhost:9696/get_action'
+
+actual_response = requests.post(url, json = ticker_info).json()
+
+if parameters == ['Close', 'macd', 'macd_diff', 'macd_signal', 'obv']:
+    expected_response_parm_dict = "{'Close': 181.17999267578125, 'macd': -1.527435403503489, 'macd_diff': -1.972018625858581, 'macd_signal': 0.44458322235509207, 'obv': -52485900}"
+    expected_response_target_date = '2024-01-05'
+elif parameters ==  ['Close', 'Volume', 'macd', 'obv']:
+    expected_response_parm_dict = "{'Close': 181.17999267578125, 'Volume': 62303300, 'macd': -1.527435403503489, 'obv': -52485900}"
+    expected_response_target_date = '2024-01-05'
+
+assert expected_response_parm_dict == actual_response['parm_dict'], "parm_dict error"
+assert expected_response_target_date == actual_response['target_date'], "target_date error"
